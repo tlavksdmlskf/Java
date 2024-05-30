8917280cd243af67471627db6af956c69cfbd89b
tlavksdmlskf <127850569+tlavksdmlskf@users.noreply.github.com>
2024-05-30 18:04:58 +0900

    Add files via upload

---
+++ nullLabel/after/Main.java
@@ -0,0 +1,17 @@
+package nullLabel.after;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Person[] people = {
+                new Person(new Label("Alice"),new Label("alice@aaa.com")),
+                new Person(new Label("James"), new Label("james@aaa.com")),
+                new Person(new Label("tomas"))
+        };
+        for(Person person : people){
+            System.out.println(person.toString());
+            person.display();
+            System.out.println();
+        }
+    }
+}
---
+++ nullLabel/after/NullLabel.java
@@ -0,0 +1,15 @@
+package nullLabel.after;
+
+public class NullLabel extends Label{
+    public NullLabel() {
+        super("(none)");
+    }
+
+    @Override
+    public boolean isNull() {
+        return true;
+    }
+
+    @Override
+    public void display(){}
+}
