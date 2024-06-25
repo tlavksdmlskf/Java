8f32ece0eda7f3512cc6e523bd293eec136d382d
tlavksdmlskf <127850569+tlavksdmlskf@users.noreply.github.com>
2024-06-25 17:39:22 +0900

    래퍼 클래스

---
+++ wrapper/WrapperClassMain.java
@@ -0,0 +1,8 @@
+package lang.wrapper;
+
+public class WrapperClassMain {
+    public static void main(String[] args) {
+        Integer newInteger = 10;
+        Integer integerObj = Integer.valueOf(10);
+    }
+}
